1afa8023e42187ac50e23cbf0102c12342906732
vi0let-byte <emmafang127@gmail.com>
2026-03-28 23:23:45 -0400

    refactored sparrow behavior. experienced sisyphean agony over rotation. KNOWN BUG: Sparrow rotates itself back to incorrect orientation after leaving feeder

--- Assets/Scripts/SparrowBehavior.cs
+++ Assets/Scripts/SparrowBehavior.cs
@@ -1,50 +1,98 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 [RequireComponent(typeof(AudioSource))]
 public class SparrowBehavior : MonoBehaviour
 {
+    public enum SparrowState {Enter, Eat, Exit}
+    public SparrowState currentState = SparrowState.Enter;
     private Animator animator;
     public GameObject ledge;
+    public int eatTime = 5;
+    private WaitForSeconds eatWait;
     public float speed = 5;
     private Vector3 startPosition;
-    private Quaternion turned; 
-    private bool enter = true;
-    private bool exit = false;
+    private bool flyAway = false;
+    private int animState = 0;
     private AudioSource chirp;
+    private bool turned = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Debug.Log("Original rotation " + transform.eulerAngles);
         animator = GetComponent<Animator>();
         startPosition = transform.position;
         chirp = GetComponent<AudioSource>();
+        eatWait = new WaitForSeconds(eatTime);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(enter)
+        switch(currentState)
         {
-            transform.LookAt(ledge.transform);
-            transform.position = Vector3.MoveTowards(transform.position, ledge.transform.position, speed * Time.deltaTime);
-        }
-        else if(exit)
-        { 
-            transform.rotation = Quaternion.Lerp(transform.rotation, turned, 1.5f*Time.deltaTime);
-            if(Quaternion.Angle(transform.rotation, turned) <= 2f)
+            case SparrowState.Enter:
             {
-                animator.speed = 1;
-                animator.SetBool("flyAway", true); 
-                transform.position = Vector3.MoveTowards(transform.position, startPosition, speed * Time.deltaTime);
-                if(Vector3.Distance(transform.position, startPosition) <= 0.01f)
-                {
-                    animator.SetBool("isStopped", true);
-                }
-                
+                Enter();
+                break;
+            }
+            case SparrowState.Eat:
+            {
+                StartCoroutine(Eat());
+                break;
+            }
+            case SparrowState.Exit:
+            {
+                Exit();
+                break;
             }
         }
+        animator.SetInteger("animState", animState);
+    }
+
+    private void Enter()
+    {
+        animState = 1;
+        transform.LookAt(ledge.transform);
+        transform.position = Vector3.MoveTowards(transform.position, ledge.transform.position, speed*Time.deltaTime);
+    }
+
+    IEnumerator Eat()
+    {
+        animState = 2;
+        yield return eatWait;
+        currentState = SparrowState.Exit;
+        yield break;
+    }
+
+    private void Exit()
+    {
+        Debug.Log("ExitTime: Flyaway is" + flyAway);
+        if(!flyAway)
+        {
+            animState = 3;
+        }
+        else
+        { 
+            animState = 1;
+            if(!turned)
+            {
+                transform.Rotate(0, 180, 0, Space.World);
+                turned = true;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, startPosition, speed*Time.deltaTime);
+        }
+        
+    }
+
+    public void SetFlyAway()
+    {
+        flyAway = true;
     }
 
     void OnTriggerEnter(Collider other)
@@ -52,23 +100,7 @@ public class SparrowBehavior : MonoBehaviour
         if(other.gameObject.CompareTag("Ledge"))
         {
             chirp.Play();
-            enter = false;
-            animator.SetBool("isEating", true);
-            Invoke("TurnExit", 5);
+            currentState = SparrowState.Eat;
         }
     }
-
-    void TurnExit()
-    {
-        animator.SetBool("isEating", false);
-        animator.speed = 0.25f;
-        animator.SetBool("isTurning", true);
-    }
-
-    void ExitStart()
-    {
-        turned = transform.rotation * Quaternion.Euler(0, 180f, 0);
-        exit = true;
-    }
-
 }
